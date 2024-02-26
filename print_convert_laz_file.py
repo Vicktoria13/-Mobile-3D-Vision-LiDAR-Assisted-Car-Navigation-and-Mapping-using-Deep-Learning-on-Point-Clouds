e3432b8007434dae1ede17289c6b3596b5a0f4c1
Vicktoria13 <Victoria-nguyen@orange.fr>
2024-02-26 14:07:36 +0100

    convert laz to txt

---
+++ print_convert_laz_file.py
@@ -0,0 +1,36 @@
+
+#%%
+import numpy as np
+import laspy
+import open3d as o3d
+import polyscope as ps
+
+
+inFile = laspy.read("/home/sdi-2023-01/Bureau/epfl/Data_pcd/85268 - M230905_223260_223320_LEFT.laz")
+coords = np.vstack((inFile.x, inFile.y, inFile.z)).transpose()
+
+
+#%%
+
+print(coords.shape)
+print("1ere ligne = ", coords[0])
+#ajouter une colonne de 0 au debut et 2 colonnes de 0 a la fin
+
+
+
+new_coords = np.hstack((np.zeros((coords.shape[0],1)), coords))
+new_coords = np.hstack((new_coords, np.zeros((coords.shape[0],2))))
+
+print(new_coords.shape)
+print("1ere ligne = ", new_coords[0])
+
+#%%
+
+def from_laz_to_txt_with_6_columns(inPath, outPath):
+    inFile = laspy.read(inPath)
+    coords = np.vstack((inFile.x, inFile.y, inFile.z)).transpose()
+    # new coordinate to easily adapt with the current code
+    
+    new_coords = np.hstack((np.zeros((coords.shape[0],1)), coords))
+    new_coords = np.hstack((new_coords, np.zeros((coords.shape[0],2))))
+    np.savetxt(outPath, new_coords, delimiter=" ", fmt="%s")
--- test_poly.py
+++ test_poly.py
@@ -1,3 +1,4 @@
+#%%
 import numpy as np
 import laspy
 import open3d as o3d
@@ -29,22 +30,15 @@ box1 = o3d.geometry.AxisAlignedBoundingBox.create_from_points(downpcd.points)
 box2 = o3d.geometry.AxisAlignedBoundingBox.create_from_points(downpcd2.points)
 
 
-dims1 = box1.get_max_bound() - box1.get_min_bound()
-bound_low1 = box1.get_min_bound()
-bound_high1 = box1.get_max_bound()
+#%%
 
-dims2 = box2.get_max_bound() - box2.get_min_bound()
-bound_low2 = box2.get_min_bound()
-bound_high2 = box2.get_max_bound()
+print("center of box 1: ", box1.get_center())
 
-
+#%%
 
 
 #polyscope
 ps.init()
-ps_box1 = ps.register_volume_grid("box1", bound_low1, dims1, [0.1, 0.1, 0.7])
-ps_box2 = ps.register_volume_grid("box2", bound_low2, dims2, [0.7, 0.1, 0.1])
-
 #taille des points  sphère de rayon 0.01
 ps.register_point_cloud("points", array, radius=0.0006, color=[0.1, 0.1, 0.7])
 ps.register_point_cloud("points2", array2, radius=0.0006, color=[0.7, 0.1, 0.1])
