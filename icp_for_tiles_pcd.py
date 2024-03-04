cc47056289a3ac44b3c9199bf498b9cb0ea589c6
Vicktoria13 <Victoria-nguyen@orange.fr>
2024-03-04 14:28:30 +0100

    test icp

--- icp_for_tiles_pcd.py
+++ icp_for_tiles_pcd.py
@@ -22,9 +22,20 @@ pcd2 = o3d.geometry.PointCloud()
 pcd1.points = o3d.utility.Vector3dVector(point_pcd1)
 pcd2.points = o3d.utility.Vector3dVector(point_pcd2)
 
+pcd1.paint_uniform_color([1, 0.706, 0])
+pcd2.paint_uniform_color([0, 0.651, 0.929])
 
+print("premier nuage a ", len(pcd1.points), " points et le deuxieme nuage a ", len(pcd2.points), " points")
 o3d.visualization.draw_geometries([pcd1, pcd2])
 
+
+
+#downsampling
+pcd1 = pcd1.voxel_down_sample(voxel_size=0.02)
+pcd2 = pcd2.voxel_down_sample(voxel_size=0.02)
+
+print("premier nuage a ", len(pcd1.points), " points et le deuxieme nuage a ", len(pcd2.points), " points")
+print("downsampling")
 
 #on applique l'ICP
 icp_res = o3d.pipelines.registration.registration_icp(pcd1, pcd2, 0.02, np.eye(4), o3d.pipelines.registration.TransformationEstimationPointToPoint(), o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=200))
