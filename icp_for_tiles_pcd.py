2467757e66e941696fda96a2da6831083f2d669a
Vicktoria13 <Victoria-nguyen@orange.fr>
2024-03-04 14:22:52 +0100

    test icp

--- icp_for_tiles_pcd.py
+++ icp_for_tiles_pcd.py
@@ -3,28 +3,31 @@ import numpy as np
 import laspy
 
 
-#on load les nuages de points sous forme 
-inFile = laspy.read("")
-coords = np.vstack((inFile.x, inFile.y, inFile.z)).transpose()
 
-other = laspy.read("")
-coords2 = np.vstack((other.x, other.y, other.z)).transpose()
+
+#on load les nuages de points sous forme txt
+
+point_pcd1 = np.loadtxt("C:/Users/jeanbaptiste/Desktop/Projet de semestre ICP/point_pcd1.txt", delimiter=",")
+point_pcd2 = np.loadtxt("C:/Users/jeanbaptiste/Desktop/Projet de semestre ICP/point_pcd2.txt", delimiter=",")
+
+#on ne garde que les colonnes 1 2 3
+point_pcd1 = point_pcd1[:,1:4]
+point_pcd2 = point_pcd2[:,1:4]
+
+
 
 pcd1 = o3d.geometry.PointCloud()
-pcd1.points = o3d.utility.Vector3dVector(coords)
-
 pcd2 = o3d.geometry.PointCloud()
-pcd2.points = o3d.utility.Vector3dVector(coords2)
+
+pcd1.points = o3d.utility.Vector3dVector(point_pcd1)
+pcd2.points = o3d.utility.Vector3dVector(point_pcd2)
 
 
 o3d.visualization.draw_geometries([pcd1, pcd2])
 
 
 #on applique l'ICP
-icp_res = o3d.pipelines.registration.registration_icp(
-    pcd1, pcd2, 0.1, np.eye(4),
-    o3d.pipelines.registration.TransformationEstimationPointToPoint(),
-    o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=200))
+icp_res = o3d.pipelines.registration.registration_icp(pcd1, pcd2, 0.02, np.eye(4), o3d.pipelines.registration.TransformationEstimationPointToPoint(), o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=200))
 
 
 
