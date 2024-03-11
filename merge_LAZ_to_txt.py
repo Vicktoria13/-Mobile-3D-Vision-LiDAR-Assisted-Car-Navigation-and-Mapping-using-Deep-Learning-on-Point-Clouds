ac4a4de702d7c27198ea95d85ad1bc032612f8bd
Vicktoria13 <Victoria-nguyen@orange.fr>
2024-03-11 14:04:24 +0100

    last

--- merge_LAZ_to_txt.py
+++ merge_LAZ_to_txt.py
@@ -19,14 +19,10 @@ pcd = o3d.geometry.PointCloud()
 pcd.points = o3d.utility.Vector3dVector(coords)
 pcd_down = pcd.voxel_down_sample(voxel_size=0.1)
 
+coords = np.asarray(pcd_down.points)
+print("coords.shape: ", coords.shape)
 
-#save as a laz file .laz
-#on sauvegarde le pcd en .laz
 
-laspy_obj = laspy.create(file_version="1.2", point_format=2)
-laspy_obj.x = pcd_down.points[:,0]
-laspy_obj.y = pcd_down.points[:,1]
-laspy_obj.z = pcd_down.points[:,2]
-laspy_obj.intensity = np.zeros(pcd_down.points.shape[0])
-laspy_obj.save("/home/sdi-2023-01/Bureau/epfl/Data_pcd/85268 - M230905_223260_223320_LEFT_RIGHT.laz")
 
+#save en txt
+np.savetxt("/home/sdi-2023-01/Bureau/epfl/Data_pcd/85268 - M230905_223260_223320_LEFT_RIGHT.txt", coords, delimiter=" ", fmt="%s")
