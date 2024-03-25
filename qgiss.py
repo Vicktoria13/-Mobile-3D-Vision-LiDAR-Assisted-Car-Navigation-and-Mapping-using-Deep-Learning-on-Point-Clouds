10a34dd25ca2514f88e8b13f00dbd5a31ed28685
Vicktoria13 <Victoria-nguyen@orange.fr>
2024-03-25 10:01:22 +0100

    qgis

--- qgiss.py
+++ qgiss.py
@@ -2,7 +2,7 @@
 from qgis.core import QgsProject, QgsVectorLayer, QgsSpatialIndex
 
 # Chemin vers le fichier GQZ contenant vos scans géoréférencés
-chemin_fichier_gqz = "/chemin/vers/votre/fichier.gqz"
+chemin_fichier_gqz = "/media/topostudent/Data1/2024spring_VictoriaZ/01_raw_data/04_CALCULS/230905/LASER/5_VECTORS/scan_map.qgz"
 
 
 #print l'attribut name de chque scan
@@ -15,7 +15,7 @@ for couche in QgsProject.instance().mapLayers().values():
     print(couche.name())
 
 # Obtenir la couche de scans à partir du projet
-couche_scans = QgsProject.instance().mapLayersByName("Nom_de_votre_couche")[0]
+couche_scans = QgsProject.instance().mapLayersByName("EXTEND_SDC_RIGHT_M230905")[0]
 
 # Créer un index spatial pour la couche de scans
 index_spatial = QgsSpatialIndex(couche_scans)
@@ -25,6 +25,47 @@ for scan in couche_scans.getFeatures():
     print(scan.attribute("name"))
 
 print("il y a ", couche_scans.featureCount(), "scans dans la couche")
+
+
+#affiche la liste des attributs disponibles pour la couche
+for field in couche_scans.fields():
+    print(field.name())
+
+#affiche les attributs du 1er scan
+print(couche_scans.getFeature(0).attributes())
+
+
+#creer une liste de pair de scans qui se chevauchent : on inclut pas le scan lui-même
+
+chevauchements = []
+list_chevauchements = []
+
+# Parcourir tous les scans
+for scan in couche_scans.getFeatures():
+
+    #scan est un objet : cela represente un scan parmi les 352 scans
+    
+    # Obtenir les identifiants des scans qui se chevauchent avec le scan actuel
+    chevauchements = index_spatial.intersects(scan.geometry().boundingBox())
+    
+    #enlever le scan actuel de la liste des scans chevauchants
+    chevauchements.remove(scan.id())
+    
+    #ajouter les paires de scans qui se chevauchent
+    for chevauchement in chevauchements:
+        list_chevauchements.append([scan.id(), chevauchement])
+    
+    #print("Scan name: ", scan.attribute("name"))
+    #print("Chevauchements: ", chevauchements)
+
+
+
+
+#boucle pour afficher les paires de scans qui se chevauchent
+for chevauchement in list_chevauchements:
+    print("Scan 1: ", couche_scans.getFeature(chevauchement[0]).attribute("name"))
+    print("Scan 2: ", couche_scans.getFeature(chevauchement[1]).attribute("name"))
+    print("=======================================")
 
 """
 # Charger le projet QGIS à partir du fichier GQZ
