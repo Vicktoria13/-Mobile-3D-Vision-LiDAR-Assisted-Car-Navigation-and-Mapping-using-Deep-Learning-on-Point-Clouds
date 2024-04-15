aa7b8b4e0951063a42b87b01a0d5689a0a25c700
Vicktoria13 <Victoria-nguyen@orange.fr>
2024-04-15 09:51:10 +0200

    f

--- divid_time.py
+++ divid_time.py
@@ -45,7 +45,11 @@ divided_points = []
 for i in range(time_divisions):
     division_indices = (infile.gps_time >= min(infile.gps_time) + i * time_step) & (infile.gps_time < min(infile.gps_time) + (i+1) * time_step)
     division_points = infile.points[division_indices]
-    divided_points.append(division_points)
+
+    #on transforme les points en xyz
+    division_points_xyz = np.vstack((division_points.x, division_points.y, division_points.z)).transpose()
+
+    divided_points.append(division_points_xyz)
 
 #on peut ensuite afficher le nombre de points dans chaque division
 for i in range(time_divisions):
@@ -53,10 +57,15 @@ for i in range(time_divisions):
 
 ## polyscope
 
+
+#divided_points est un tableau de tableaux de points
+print("divided_points est un tableau de ",type(divided_points[0]),"de taille ",len(divided_points[0]))
 #register with radius and random color
 radius = 0.0003
 
 ps.init()
+
 for i in range(time_divisions):
-    ps.register_point_cloud("points"+str(i), sorted_point_xyz, radius=radius, color=[np.random.rand(), np.random.rand(), np.random.rand()])
+    ps.register_point_cloud("points"+str(i), divided_points[i], radius=radius)
+
 ps.show()
